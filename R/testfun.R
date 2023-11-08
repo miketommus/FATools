b04b794e59655846d2344e1916648ff5af584191
miketommus <64033639+miketommus@users.noreply.github.com>
2023-11-08 14:09:30 -0800

    Initial commit of R package framework with test func. added.

---
+++ R/testfun.R
@@ -0,0 +1,15 @@
+#' Add some numbers
+#'
+#' @param x A number.
+#' @param y Another number.
+#'
+#' @return The sum.
+#' @export
+#'
+#' @examples
+#' x <- 5
+#' y <- 6
+#' testfun(x, y) # returns 11
+testfun <- function(x, y) {
+  x + y
+}
