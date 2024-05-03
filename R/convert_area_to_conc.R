e9ef4d79e1c125392e46a124e8ab842dcfd8d79a
miketommus <64033639+miketommus@users.noreply.github.com>
2024-05-02 17:01:12 -0700

    Add function to convert areas to concentration

--- R/convert_area_to_conc.R
+++ R/convert_area_to_conc.R
@@ -15,21 +15,54 @@
 #' @export
 #'
 #' @examples
-#' # Add some examples
+#'# example compound peak area data
+#'data <- data.frame(
+#'  runif(5, min = 1000, max = 3000000),
+#'  runif(5, min = 1000, max = 3000000),
+#'  runif(5, min = 1000, max = 3000000),
+#'  runif(5, min = 1000, max = 3000000),
+#'  runif(5, min = 1000, max = 3000000),
+#'  runif(5, min = 1000, max = 3000000)
+#')
+#'names(data) <- c("8:0", "10:0", "12:0", "14:0", "i-15:0", "15:0")
+#'
+#'# example response factor table
+#'rf_table <- data.frame(
+#'  fa = c("10:0", "8:0", "12:0", "14:0", "i-15:0", "15:0"),
+#'  rf = c(15000, 15000, 16000, 12000, 0, 10000)
+#')
+#'
+#'# example response factor map
+#'rf_map <- data.frame(
+#'  fa = c("8:0", "10:0", "12:0", "14:0", "i-15:0", "15:0"),
+#'  ref_fa = c(NA, NA, NA, NA, "15:0", NA)
+#')
+#'
+#'# Convert areas to concentration
+#'convert_area_to_conc(data, rf_table, rf_map = rf_map)
 #'
 convert_area_to_conc <- function(data, rf_table, ..., rf_map = NULL) {
+
+  # Check that RF table matches FA in data
+  if (!all(sort(colnames(data)) == sort(rf_table$fa))) {
+    stop("Compounds in rf_table do not match compounds in data.")
+  }
+
   # Only runs if rf_map is provided
   if (!is.null(rf_map)) {
-    # For ea row in rf_table that is 0||NA, use rf_map to add RF from
-    # representative FA into rf_table
+    # For ea row in rf_table
     for (i in 1:nrow(rf_table)) {
+      # If 0||NA, use rf_map to add RF from representative FA into rf_table
       if (rf_table$rf[i] == 0 || is.na(rf_table$rf[i])) {
-        rf_table$rf[i] <- rf_table$rf[
-          match(
-            rf_map$ref_fa[match(rf_table$fa[i], rf_map$fa)],
-            rf_table$fa
-          )
-        ]
+        rf_map_indx <- match(rf_table$fa[i], rf_map$fa)
+        # If FA is found in rf_map
+        if (!is.na(rf_map_indx)) {
+          rf_table$rf[i] <- rf_table$rf[
+            match(rf_map$ref_fa[rf_map_indx], rf_table$fa)
+          ]
+        } else {
+          stop(paste0("Compound ", rf_table$fa[i], " was not found in rf_map."))
+        }
       } else {
         # do nothing
       }
@@ -51,18 +84,3 @@ convert_area_to_conc <- function(data, rf_table, ..., rf_map = NULL) {
   # Return concentration data
   data
 }
-
-# # test data
-# data <- test_sample_areas
-# # test rf_table
-# rf_table <- FATools::calc_gc_response_factor(
-#   test_ext_std_areas,
-#   c(15, 50, 100, 250),
-#   test_nucheck_566c
-# )
-# # test rf map
-# rf_map <- test_rf_map
-
-test_results <- convert_area_to_conc(data, rf_table, rf_map = rf_map)
-test_results2 <- convert_area_to_conc(data, rf_table)
-n <- test_results == test_results2
